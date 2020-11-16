d06c6d80f7348cec382b2289962b182bb403c4f8
bdmostafa <mdmostafaalmahmud@gmail.com>
2020-11-16 12:15:31 +0600

    sidebar search

--- src/components/Sidebar/Sidebar.js
+++ src/components/Sidebar/Sidebar.js
@@ -4,15 +4,17 @@ import PermIdentityIcon from "@material-ui/icons/PermIdentity";
 import SupervisorAccountIcon from "@material-ui/icons/SupervisorAccount";
 import SettingsIcon from "@material-ui/icons/Settings";
 import { IconButton } from "@material-ui/core";
-import SearchIcon from '@material-ui/icons/Search';
+import SearchIcon from "@material-ui/icons/Search";
 
 const Sidebar = () => {
   return (
     <div className="sidebar">
       <h2>Messages</h2>
       <div className="sidebar__search">
-        <SearchIcon />
-        <input type="search"/>
+        <div className="sidebar__searchContainer">
+          <SearchIcon />
+          <input type="search" placeholder="Search people or messages" />
+        </div>
       </div>
       <div className="sidebar__headerIcon">
         <IconButton>
