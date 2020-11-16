a02d5140350329de81cf28afc34dda0aa65841a4
bdmostafa <mdmostafaalmahmud@gmail.com>
2020-11-17 00:16:30 +0600

    pusher.js - realtime mongoDB data transactions

--- src/App.js
+++ src/App.js
@@ -1,10 +1,24 @@
-import './App.css';
-import Chat from './components/Chat/Chat';
-import Info from './components/Info/Info';
-import LeftSidebarIcon from './components/LeftSidebarIcon/LeftSidebarIcon';
-import Sidebar from './components/Sidebar/Sidebar';
+import React, { useEffect } from "react";
+import "./App.css";
+import Chat from "./components/Chat/Chat";
+import Info from "./components/Info/Info";
+import LeftSidebarIcon from "./components/LeftSidebarIcon/LeftSidebarIcon";
+import Sidebar from "./components/Sidebar/Sidebar";
+import Pusher from 'pusher-js';
 
 function App() {
+
+  useEffect(() => {
+    const pusher = new Pusher("6fba44253488d9d7fcc8", {
+      cluster: "ap1",
+    });
+
+    var channel = pusher.subscribe("messages");
+    channel.bind("inserted", (data) => {
+      alert(JSON.stringify(data));
+    });
+  }, []);
+
   return (
     <div className="App">
       <div className="app__body">
@@ -13,7 +27,6 @@ function App() {
         <Chat />
         <Info />
       </div>
-      
     </div>
   );
 }
